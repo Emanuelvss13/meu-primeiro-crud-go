c1b5dc13ce15ff14a83e13fd9f8a0d698f5f974f
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-03-15 14:31:23 -0300

    feat: find, update and delete controller files

--- src/controller/find-user.go
+++ src/controller/find-user.go
@@ -1,9 +1,18 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Emanuelvss13/meu-primeiro-crud-go/src/configuration/Error"
+	"github.com/gin-gonic/gin"
+)
 
 func FindUserByID(c *gin.Context) {
 
+	err := Error.NewInternalServerError("Error")
+
+	c.JSON(err.Code, err)
+
 }
 
-func FindUserByEmail(c *gin.Context) {}
+func FindUserByEmail(c *gin.Context) {
+
+}
