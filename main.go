3c308b562c99fc64dca8575d425698efc4afe8b7
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-03-15 14:29:52 -0300

    feat: zap logger

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/Emanuelvss13/meu-primeiro-crud-go/src/configuration/logger"
 	"github.com/Emanuelvss13/meu-primeiro-crud-go/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("About to start application")
 
 	err := godotenv.Load()
 
